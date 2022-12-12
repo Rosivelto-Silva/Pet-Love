ae1a04ee4e5f1a7371ab0e98f34145599c307895
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2022-12-12 15:19:46 -0300

    inica o projeto

---
+++ src/main/java/br/com/petlove/Clientepetlove/ClientePetloveApplication.java
@@ -0,0 +1,13 @@
+package br.com.petlove.Clientepetlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePetloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePetloveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petlove/Clientepetlove/ClientePetloveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petlove.Clientepetlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
